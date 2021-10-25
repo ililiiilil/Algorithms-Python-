0654af36743519ae4b76bad9f0e3b4336ef668c7
ililiiilil <gjwnsgod1@naver.com>
2021-10-25 20:13:07 +0900

    study

---
+++ BaekJoon/BOJ10886.py
@@ -0,0 +1,14 @@
+n = int(input())
+cnt = 0
+
+for i in range(n):
+    a = int(input())
+    if a == 1:
+        cnt += 1 
+    else:
+        cnt -= 1 
+
+if cnt > 0:
+    print("Junhee is cute!")
+else:
+    print("Junhee is not cute!")        
